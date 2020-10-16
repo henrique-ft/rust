e2637ecd47cc0bbccbb568e6b6cc59d1f7a9bc65
Henrique Fernandez Teixeira <hriqueft@gmail.com>
2020-10-16 15:39:35 -0300

    first commit

---
+++ hello.rs
@@ -0,0 +1,19 @@
+// Rust:
+//
+// is a modern C or C++ and do what they do but much better
+// a low level language that garantee memory-safety
+// has a rich type system and ownership model guarantee memory-safety and thread-safety
+// is blazingly fast and memory-efficient: with no runtime or garbage collector
+// the code will not compile if using memory in a wrong way
+// dont have null value or exceptions! (use the type system to guarantee all)
+//
+// Environment:
+//
+// rustc <- compiler
+// cargo <- pakage manager
+// rustup <- versions manager
+
+#[name]
+fn main() {
+    println!("Hello World")
+}
