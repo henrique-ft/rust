01a5de08221505983d47cd50b11e775759980774
henrique.teixeira <henrique.teixeira@locaweb.com.br>
2020-10-27 18:27:47 -0300

    macros

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -7,7 +7,17 @@ fn main() {
     // The println! is a macro (not a function), every time we see a "!" in the final of function, that means that
     // this is a macro
     println!("Hello, world!");
-    // Macros dont need to have a strict parameter type, we can send anything to macro parameter
+    // Macros dont need to have a strict parameter type, we can send anything to macro parameter.
+    // We can see how this function is in internals by using "cargo-expand" lib:
+    //  {
+         //::std::io::_print(::core::fmt::Arguments::new_v1(
+             //&["Hello, world!\n"],
+             //&match () {
+                 //() => [],
+             //},
+         //));
+     //};
+
 
     // Interpolation
     println!("My {} is {}", "age", 24);
